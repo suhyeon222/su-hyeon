f4bd64eb5e25a1341d59285ed76b784719c5b940
suhyeon222 Su hyeon <mshchc@naver.com>
2024-06-11 05:49:12 +0000

    dd

---
+++ script.js
@@ -0,0 +1,24 @@
+function navigateToPage(pageNumber) {
+    switch(pageNumber) {
+        case 1:
+            window.location.href = 'page1.html';
+            break;
+        case 2:
+            window.location.href = 'page2.html';
+            break;
+        case 3:
+            window.location.href = 'page3.html';
+            break;
+        case 4:
+            window.location.href = 'page4.html';
+            break;
+        case 5:
+            window.location.href = 'page5.html';
+            break;
+        case 6:
+            window.location.href = 'page6.html';
+            break;
+        default:
+            window.location.href = '#';
+    }
+}
